da6054465e18eb7defcb031273ba52e31984923e
RouenPremium <rouen.premium@gmail.com>
2019-02-27 10:04:59 +0700

    All plugin project

--- keenetic/src/components/HelloWorld.vue
+++ keenetic/src/components/HelloWorld.vue
@@ -6,16 +6,16 @@
     >
       <v-flex xs12>
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/keen.svg')"
           class="my-3"
           contain
-          height="200"
+          height="400"
         ></v-img>
       </v-flex>
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          KeenEtic
         </h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
